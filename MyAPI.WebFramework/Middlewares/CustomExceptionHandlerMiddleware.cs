18dcf0b1b62f68cc01576de3f8f917e9163c6fb6
Mohammad Hossein Rostampour <mh.rostampour@gmail.com>
2022-08-19 12:04:10 +0430

    CustomExceptionHandlerMiddleware

---
+++ MyAPI.WebFramework/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using MyAPI.WebFramework.api;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAPI.WebFramework.Middlewares
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<CustomExceptionHandlerMiddleware> logger;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next ,
+            ILogger<CustomExceptionHandlerMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError(ex, "خطایی رخ  داده است");
+
+                var apiResult = new ApiResult(false , ApiResultStatusCode.ServerError);
+                var json = JsonConvert.SerializeObject(apiResult);
+
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
